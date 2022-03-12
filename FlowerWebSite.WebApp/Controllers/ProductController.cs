a4ccaa915e717d88be040c9c64d464f16d90138d
Meltem Candan <meltemcandan52@gmail.com>
2022-03-12 23:47:36 +0300

    footer da değişiklikler yapıldı.

---
+++ FlowerWebSite.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerWebSite.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Papatya()
+        {
+            return View();
+        }
+
+        public IActionResult KirmiziGul()
+        {
+            return View();
+        }
+        public IActionResult YildizCicegi()
+        {
+            return View();
+        }
+        public IActionResult SariLale()
+        {
+            return View();
+        }
+    }
+}
