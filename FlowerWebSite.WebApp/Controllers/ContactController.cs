3707eb9ff3044a66976cf6a7734052ef675673aa
Meltem Candan <meltemcandan52@gmail.com>
2022-03-17 23:51:55 +0300

    İletişim sayfası için form örneği oluşturuldu.

---
+++ FlowerWebSite.WebApp/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using FlowerWebSite.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerWebSite.WebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Save(ContactViewModel viewModel)
+        {
+
+        }
+    }
+}
---
+++ FlowerWebSite.WebApp/Models/ContactViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FlowerWebSite.WebApp.Models
+{
+    public class ContactViewModel
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+    }
+}
